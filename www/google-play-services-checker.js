92de85c0cf3d44b68e5a097855414917421b375b
oren-oro <88835671+oren-oro@users.noreply.github.com>
2025-01-14 16:38:21 +0200

    Update google-play-services-checker.js

--- www/google-play-services-checker.js
+++ www/google-play-services-checker.js
@@ -3,7 +3,7 @@ var emptyFnc = function () {
 module.exports = {
   check: function (successCallback, errorCallback) {
     cordova.exec(successCallback || emptyFnc, errorCallback || emptyFnc, "GooglePlayServicesChecker", "check", []);
-  }
+  },
   fix: function (successCallback, errorCallback) {
     cordova.exec(successCallback || emptyFnc, errorCallback || emptyFnc, "GooglePlayServicesChecker", "fix  ", []);
   }
