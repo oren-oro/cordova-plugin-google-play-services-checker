30a5c012c5a9d986ae0e0e3b477b9a26b93e01d5
oren-oro <88835671+oren-oro@users.noreply.github.com>
2025-01-15 12:35:27 +0200

    Update google-play-services-checker.js
    Add ConnectionResult values

--- www/google-play-services-checker.js
+++ www/google-play-services-checker.js
@@ -6,5 +6,13 @@ module.exports = {
   },
   fix: function (successCallback, errorCallback) {
     cordova.exec(successCallback || emptyFnc, errorCallback || emptyFnc, "GooglePlayServicesChecker", "fix", []);
+  },
+  ConnectionResult: {
+      SUCCESS: 0,
+      SERVICE_MISSING: 1,
+      SERVICE_VERSION_UPDATE_REQUIRED: 2,
+      SERVICE_DISABLED: 3,
+      SERVICE_INVALID: 9,
+      SERVICE_UPDATING: 18,
   }
 };
